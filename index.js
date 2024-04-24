f491033e417971249bd9ef7d0ace842fe16c1c07
susmithagundala <166511796+susmithagundala@users.noreply.github.com>
2024-04-24 09:03:10 +0000

    check out the code

---
+++ index.js
@@ -0,0 +1,6 @@
+const addDays = require('date-fns/addDays')
+const getDaysAfterXDays = days => {
+  const result = addDays(new Date(2020, 7, 22), days)
+  return `${result.getDate()}-${result.getMonth() + 1}-${result.getFullYear()}`
+}
+module.exports = getDaysAfterXDays
